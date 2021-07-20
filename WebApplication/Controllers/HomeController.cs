e52d5fb9171fae494323e816f09f897eb7b14804
SiddhiBhand <bhand.siddhi@gmail.com>
2021-07-20 21:56:43 +0530

    initial commit

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+      //  [ResponseCache(VaryByHeader ="User-Agent",Duration =30)]
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public ActionResult Index()
+        {
+            List<User> obj = new List<User>();
+            obj.Add(new User() { id=101,Name="Siddhi"});
+            obj.Add(new User() { id = 102, Name = "Sayali" });
+            obj.Add(new User() { id = 103, Name = "Trupti" });
+            obj.Add(new User() { id = 104, Name = "Chaitrali" });
+
+            ViewData["UserInfo"] = obj;
+
+            ViewBag.obj = obj;
+
+            TempData["UserInfo"] = obj;
+
+            ViewBag.Dt = DateTime.Now.ToString("MM/yy hh:mm:ss");
+            return View();
+        }
+        public FileContentResult FileContentEx()
+        {
+            var pdf = System.IO.File.ReadAllBytes("wwwroot/Assignments.pdf");
+            return new FileContentResult(pdf,"application/pdf");
+        }
+        public ViewResult ViewResultEx()
+        {
+            return View();
+        }
+
+        public JsonResult JsonResultEx()
+        {
+            string[] fruits = { "apple", "mango" };
+            return Json(fruits);
+        }
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class User
+    {
+        public int id;
+        public string name;
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
